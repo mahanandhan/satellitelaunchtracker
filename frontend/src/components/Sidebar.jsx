7383d5521cfda45a1ece9142bb045bcf26c03a4f
mahanandhan <mahanandhanmanish@gmail.com>
2025-06-22 22:44:33 +0530

    added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   const navigate = useNavigate();
   const handleLogout = async () => {
     try {
-      const response = await axios.post('https://satellitelaunchtracker-1.onrender.com/api/auth/logout', { withCredentials: true });
+      const response = await axios.post('https://satellitelaunchtracker-1.onrender.com/api/auth/logout');
       if (response.status === 200) {
         alert("Logged out successfully");
         navigate('/login');
