d9cf886293ca9eaa24b34d47be3186d3467c45ef
mahanandhan <mahanandhanmanish@gmail.com>
2025-06-22 15:58:28 +0530

    added

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -4,7 +4,7 @@ export const generateTokenAndSetCookies = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '15d'});
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
+        secure: true,
         sameSite: 'None',
         maxAge: 15 * 24 * 60 * 60 * 1000
     })
